05c54218a39e1158619a99b591f94376df609149
healmeshij <junsongai@163.com>
2026-04-06 18:32:46 +0800

    商家页面实现

--- main.c
+++ main.c
@@ -6,7 +6,18 @@ enum{
 	customer,
 	//}role;
 	};
-int main(int argc, const char *argv[]){
+
+//商家数据类型
+enum{
+	list = 1,
+	modify,
+	purchase,
+	sales,
+	quit,
+};
+
+void menu_merchant();
+int main(int argc, const char *argv[]){/*{{{*/
 	int role;
 	while (1){
 		printf("|******************************|\n");
@@ -15,6 +26,7 @@ int main(int argc, const char *argv[]){
 		scanf("%d", &role);
 		if (role == merchant){
 			printf("it is merchant\n");
+			menu_merchant();
 		}
 		else if (role == customer){
 			printf("it is customer\n");
@@ -24,4 +36,44 @@ int main(int argc, const char *argv[]){
 		}
 	}
 	return 0;
+}/*}}}*/
+void menu_merchant(){
+	int m;
+MENU_MERCHANT:
+	printf("|******************************|\n");
+	printf("|*        1.查看库存          *|\n");
+	printf("|*        2.修改商品          *|\n");
+	printf("|*        3.采购订单          *|\n");
+	printf("|*        4.查看销售额        *|\n");
+	printf("|*        5.退出              *|\n");
+	printf("|******************************|\n");
+	scanf("%d", &m);
+	if (m == list){
+		printf("list\n");
+		goto MENU_MERCHANT;
+	}
+	else if (m == modify)
+	{
+		printf("modify\n");
+		goto MENU_MERCHANT;
+	}
+	else if (m == purchase)
+	{
+		printf("purchase\n");
+		goto MENU_MERCHANT;
+	}
+	else if (m == sales)
+	{
+		printf("sales\n");
+		goto MENU_MERCHANT;
+	}
+	else if (m == quit)
+	{
+	}
+	else 
+	{
+		printf("your input error\n");
+		goto MENU_MERCHANT;
+	}
+
 }
